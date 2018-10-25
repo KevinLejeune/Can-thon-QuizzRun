5340c95484cd53f2afdba5d9de48e7d56b11f9d5
CAQUINEAU Aline <caquineaualine@live.fr>
2018-10-25 17:09:54 +0200

    essai

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// $(".bg-danger").hide();
+//   $(".bg-secondary").hide();
+//   $(".bg-info").hide();
+//   $(".bg-dark").hide();
+//
+
+  function(){
+    $("#check").clickval("#txt");
+          var essai = $("#txt").val();
+              alert(essai);
+
+  });
+  }
